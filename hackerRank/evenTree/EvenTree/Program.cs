c4641c1d9f7dc66038c79775e14e5379a4437b49
jcastrigano <jcastrigano@dakotasoft.com>
2017-07-29 14:46:15 -0400

    Solution to EvenTree Graph Theory DFS problem.

--- hackerRank/evenTree/EvenTree/Program.cs
+++ hackerRank/evenTree/EvenTree/Program.cs
@@ -8,17 +8,74 @@ namespace EvenTree
 {
     class Program
     {
+
+        static int answer = 0;
+
         static void Main(string[] args)
+        {
+            var adjencyList = new Dictionary<int, List<int>>();
+
+            BuildAdjencyList(adjencyList);
+
+            PrintAdjencyList(adjencyList);
+
+            dfs(adjencyList.Keys.First(), adjencyList);
+
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+
+        private static int dfs(int node, Dictionary<int, List<int>> adjencyList)
+        {
+            Console.WriteLine("Searching node: " + node);
+
+            int numOfVertex = 0;
+
+            int childNodeCount = 0;
+            if (adjencyList.ContainsKey(node))
+            {
+                childNodeCount = adjencyList[node].Count;
+            }
+
+            for (int i = 0; i < childNodeCount; i++)
+            {
+                int childNode = adjencyList[node][i];
+
+                int num_nodes = dfs(childNode, adjencyList);
+
+                if (num_nodes % 2 == 0)
+                {
+                    Console.WriteLine("Node number: " + node);
+                    answer++;
+                    Console.WriteLine("Subtree found at [{0}] with root child node [{1}] and has a total of [{2}] nodes", node, childNode, num_nodes);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    numOfVertex += num_nodes;
+                }
+            }
+            return numOfVertex + 1;
+        }
+
+        private static void PrintAdjencyList(Dictionary<int, List<int>> adjencyList)
+        {
+            foreach (var pointer in adjencyList)
+            {
+                Console.Write(pointer.Key + " => ");
+                pointer.Value
+                    .ForEach(value => Console.Write(value + " "));
+                Console.WriteLine();
+            }
+        }
+
+        private static void BuildAdjencyList(Dictionary<int, List<int>> adjencyList)
         {
             string[] setUpInput = Console.ReadLine().Split();
 
             int numberOfNodes = Int32.Parse(setUpInput[0]);
             int numberOfEdges = Int32.Parse(setUpInput[1]);
 
-
-            var adjencyList = new Dictionary<int, List<int>>();
-
-            //set up pointers of adjency list
             for (int i = 0; i < numberOfEdges; i++)
             {
                 string[] edge = Console.ReadLine().Split();
@@ -34,24 +91,10 @@ namespace EvenTree
                 {
                     adjencyList[sourceNode].Add(destinationNode);
                 }
-
-
             }
-            //Instead of printing out like we did below,
-            //we just need to check and see if the number
-            //of references for each pointer is even.
-            //If it is not, print 
+
             Console.ReadKey();
             Console.WriteLine(" ");
-            foreach (var pointer in adjencyList)
-            {
-                Console.Write(pointer.Key + " => ");
-                pointer.Value
-                    .ForEach(value => Console.Write(value + " "));
-                Console.WriteLine();
-            }
-
-            Console.ReadLine();
         }
     }
 }
